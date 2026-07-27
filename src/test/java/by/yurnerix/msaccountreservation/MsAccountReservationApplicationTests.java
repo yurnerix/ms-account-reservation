00bfd731d5e794fdb54e1c6e6fd26e11dcee4e0e
Yuriy <schexin.hoho@gmail.com>
2026-07-27 12:43:34 +0300

    UPG-1. Скелет приложения. БД + Spring Data JPA

---
+++ src/test/java/by/yurnerix/msaccountreservation/MsAccountReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package by.yurnerix.msaccountreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAccountReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
